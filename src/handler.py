8a2cc0732e25b575ec76212e4b5cbd9d253c535f
isaac parra <isaacparra@MacBook-Pro-de-isaac.local>
2023-02-18 10:00:24 -0300

    test lambda serveless

--- src/handler.py
+++ src/handler.py
@@ -1,15 +1,35 @@
-import json
+import requests
 
-def proxy_server():
+def lambda_handler(event, context):
 
+    # Get data original request
+    request = event['requestContext']['http']
+    method = request['method']
+    path = request['path']
 
+    event['headers'].pop('host')
+    headers = event['headers']
+    body = request.get('body', None)
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+    # Build url
+    url = 'https://api.mercadolibre.com' + path
+    print(url)
+
+    # Send data to target server
+    resp = requests.request(
+        method=method,
+        url=url,
+        headers=headers)
+
+    # return request to client
+    headers = dict(resp.headers)
+    body = resp.content
+
+    print(resp)
+    response = {
+        'statusCode': resp.status_code,
+        'headers': headers,
+        'body': body
     }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
     return response
+
